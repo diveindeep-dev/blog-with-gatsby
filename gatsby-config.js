de8c4c2fdf2ac4e44c4deee9233adef28e0ef09d
hakyung <diveindeep.dev@gmail.com>
2022-06-28 21:55:53 +0900

    Implement index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,34 @@
+const config = require('./contents/config');
+
 module.exports = {
-  plugins: ['gatsby-plugin-styled-components'],
+  siteMetadata: {
+    title: config.siteTitle,
+    author: config.author,
+    nav: config.siteNav,
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'contents',
+        path: `${__dirname}/contents`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 700,
+            },
+          },
+        ],
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,74 @@
-import * as React from 'react';
+import React from 'react';
+import Layout from '../layouts';
+import TopSection from '../components/TopSection';
+import { graphql, Link } from 'gatsby';
 
-const IndexPage = () => {
-  return <div>Blog with Gatsby</div>;
+const IndexPage = (props) => {
+  const { siteData, latestPost, posts, notes } = props.data;
+
+  const listItem = latestPost.edges.map((edge, i) => {
+    const { fields, frontmatter } = edge.node;
+
+    const tagList = frontmatter.tags.map((tag, i) => {
+      return <li key={i}>{tag}</li>;
+    });
+
+    return (
+      <li key={i}>
+        <Link to={`/blog/${fields.slug}`}>{frontmatter.title}</Link>
+        <ul>{tagList}</ul>
+      </li>
+    );
+  });
+
+  return (
+    <Layout>
+      <TopSection
+        title={siteData.siteMetadata.title}
+        count={{ post: posts.totalCount, note: notes.totalCount }}
+      />
+      <section>
+        <h2>Latest Blog Posts</h2>
+        <ol>{listItem}</ol>
+      </section>
+    </Layout>
+  );
 };
 
+export const pageQuery = graphql`
+  query IndexQuery {
+    siteData: site {
+      siteMetadata {
+        title
+      }
+    }
+
+    latestPost: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { nav: { eq: "blog" } } }
+      limit: 5
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            tags
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+
+    posts: allMarkdownRemark(filter: { frontmatter: { nav: { eq: "blog" } } }) {
+      totalCount
+    }
+
+    notes: allMarkdownRemark(filter: { frontmatter: { nav: { eq: "note" } } }) {
+      totalCount
+    }
+  }
+`;
 export default IndexPage;
